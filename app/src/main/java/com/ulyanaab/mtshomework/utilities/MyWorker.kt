2c88f4dccebf82071d4b56ef3cb43a0f5db288c1
Ulyana <abram.jully@gmail.com>
2021-08-23 09:51:18 +0700

    fix variables

--- app/src/main/java/com/ulyanaab/mtshomework/utilities/MyWorker.kt
+++ app/src/main/java/com/ulyanaab/mtshomework/utilities/MyWorker.kt
@@ -4,20 +4,25 @@ import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.ulyanaab.mtshomework.model.common.AppDatabase
+import com.ulyanaab.mtshomework.model.dataSource.local.LocalMovieDataSource
+import com.ulyanaab.mtshomework.model.dataSource.local.RoomLocalMovieDataSource
 import com.ulyanaab.mtshomework.model.dataSource.remote.MoviesDataSource
 import com.ulyanaab.mtshomework.model.dataSource.remote.RetrofitMoviesDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
 
-    val dataSource: MoviesDataSource = RetrofitMoviesDataSource()
-    val dao = AppDatabase.getInstance(context).movieDao()
+    private val dataSource: MoviesDataSource = RetrofitMoviesDataSource()
+    private val localDataSource: LocalMovieDataSource = RoomLocalMovieDataSource(context)
 
     override fun doWork(): Result {
         return try {
             val movies = dataSource.getMovies()
 
-            dao.clear()
-            dao.addAll(movies)
+            localDataSource.clear()
+            localDataSource.addAll(movies)
 
             Result.success()
         } catch (_: Exception) {
